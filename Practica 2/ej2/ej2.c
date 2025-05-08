1e7a744f10e3b0eb9f3356d96fe569316a378c12
FacundoGerli <facundogerli@hotmail.com>
2025-05-08 02:01:53 -0300

    ejercicio 3 y reorganizacion

--- Practica 2/ej2/ej2.c
+++ Practica 2/ej2/ej2.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <pthread.h>
+#include <sys/time.h>
 
 
 //Dimension por defecto de las matrices
-int N=100, T. blockSize;
+int N=100, blockSize, T=1;
+double *A,*B,*C;
 
 //Para calcular tiempo
 double dwalltime(){
@@ -14,22 +17,38 @@ double dwalltime(){
         sec = tv.tv_sec + tv.tv_usec/1000000.0;
         return sec;
 }
-void* mulmatriz(void* ptr){
+void* multmatriz(void* ptr){
     int suma;
     int id = *(int*) ptr;
+
+    int inicio = id * blockSize;
+    int fin = inicio + blockSize;
+    if (id == T - 1) {
+        fin = N;
+    }
+    for(int i = inicio; i < fin;i++){
+        for(int j = 0; j < N; j++){
+            suma = 0;
+            for(int k = 0; k < N; k++){
+                suma += A[i*N+k] * B[k*N+j];
+            }
+            C[i*N+j] = suma;
+        }
+    }
+    pthread_exit(NULL);
 }
 
 int main(int argc,char*argv[]){
- double *A,*B,*C;s
+ int *IDS;
  int i,j,k;
  int check=1;
  double timetick;
  double suma;
 
  //Controla los argumentos al programa
- if ((argc != 3) || ((N = atoi(argv[1])) <= 0) || (T = atoi(arg[2])) <= 0)
+ if ((argc != 3) || ((N = atoi(argv[1])) <= 0) || ((T = atoi(argv[2])) <= 0))
   {
-    printf("\nUsar: %s n\n  n: Dimension de la matriz (nxn X nxn)\n", argv[0]);
+    printf("\nUsar: %s n t\n  n: Dimension de la matriz (nxn X nxn)\n  t: Numero de hilos\n", argv[0]);
     exit(1);
   }
 
@@ -37,9 +56,9 @@ int main(int argc,char*argv[]){
   A=(double*)malloc(sizeof(double)*N*N);
   B=(double*)malloc(sizeof(double)*N*N);
   C=(double*)malloc(sizeof(double)*N*N);
+  IDS =(int *)malloc(sizeof(int)*T);
 
   blockSize = N/T;
-  int ids[T];
   pthread_t threads[T];
 
  //Inicializa las matrices A y B en 1, el resultado sera una matriz con todos sus valores en N
@@ -51,15 +70,15 @@ int main(int argc,char*argv[]){
   }   
   
   for(int i = 0; i < T; i++){
-    ids[i] = i
-    pthread_create(&threads[i], NULL, multmatriz,&ids[i]);
+    IDS[i] = i;
+    pthread_create(&threads[i], NULL, multmatriz,&IDS[i]);
   }
 
- //Realiza la multiplicacion
+ //Realiza la multiplicacion./
 
   timetick = dwalltime();
   for( i=0; i < T; i++){
-    pthread_join(&threads[i], NULL);
+    pthread_join(threads[i], NULL);
   }
 
  printf("Multiplicacion de matrices de %dx%d. Tiempo en segundos %f\n",N,N, dwalltime() - timetick);
@@ -77,6 +96,7 @@ int main(int argc,char*argv[]){
    printf("Multiplicacion de matrices resultado erroneo\n");
   }
 
+ free(IDS);
  free(A);
  free(B);
  free(C);
---
+++ Practica 2/ej3/ej3.c
@@ -0,0 +1,88 @@
+/*Desarrolle un algoritmo paralelo que cuente la cantidad de veces que un 
+elemento X aparece dentro de un vector de N elementos enteros.  Al finalizar,
+ la cantidad de ocurrencias del elemento X debe quedar en una variable llamada
+ocurrencias. Para la sincronización emplee mutex-locks. Pruebe con diversos tamaños de 
+N y T={2,4,8}. Analice el rendimiento.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/time.h>
+int *A;
+int N=100, T=1, size = 0, globalOcurrencias = 0, X = 1;
+pthread_mutex_t mutexOcurrencias;
+//Para calcular tiempo
+double dwalltime(){
+    double sec;
+    struct timeval tv;
+
+    gettimeofday(&tv,NULL);
+    sec = tv.tv_sec + tv.tv_usec/1000000.0;
+    return sec;
+}
+void *contOcurrencias(void* ptr){
+    int id = *(int *) ptr;
+    int inicio = id * size;
+    int fin = inicio + size;
+    int localOcurrencias = 0;
+
+    if (id == T-1) {
+        fin = N;
+    }
+    
+    for(int i = inicio; i < fin; i++){
+        if(A[i] == X)
+        localOcurrencias ++;
+    }
+    pthread_mutex_lock(&mutexOcurrencias);
+    globalOcurrencias += localOcurrencias;
+    pthread_mutex_unlock(&mutexOcurrencias);
+
+
+
+}
+int main(int argc,char*argv[]){
+
+    //Controla los argumentos al programa
+    if ((argc != 3) || ((N = atoi(argv[1])) <= 0) || ((T = atoi(argv[2])) <= 0))
+     {
+       printf("\nUsar: %s n t\n  n: Dimension del vector \n  t: Numero de hilos\n", argv[0]);
+       exit(1);
+     }
+
+     int check=1;
+     double timetick;
+     int *IDS, i; 
+     size = N/T;
+    //Aloca memoria para el vector
+    A=(int*)malloc(sizeof(int)*N);
+    IDS =(int *)malloc(sizeof(int)*T);
+    //Inicializa el vector
+    for (i=0; i<N; i++){
+        A[i] = 1;
+    }
+
+    //Crea los hilos
+    pthread_t threads[T];
+    pthread_mutex_init(&mutexOcurrencias, NULL);
+     for(int i = 0; i < T; i++){
+        IDS[i] = i;
+        pthread_create(&threads[i], NULL, contOcurrencias,&IDS[i]);
+      }
+    
+     //Realiza la multiplicacion./
+    
+      timetick = dwalltime();
+      for( i=0; i < T; i++){
+        pthread_join(threads[i], NULL);
+      }
+
+    
+     printf("Conteo de ocurrencias en el vector de tamaño %d. Tiempo en segundos %f\n",N, dwalltime() - timetick);
+     printf("Elemento %d encontrado %d veces\n", X, globalOcurrencias);
+
+     free(A);
+     free(IDS);
+     pthread_mutex_destroy(&mutexOcurrencias);
+     exit(0);
+    }
